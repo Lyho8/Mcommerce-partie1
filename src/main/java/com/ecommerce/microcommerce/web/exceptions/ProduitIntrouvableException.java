28ac92d56e190ae90280924fac43d38578800a2c
Pluot <aurelien@pluot.fr>
2018-12-12 14:44:34 +0100

    Corrections warnings.

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitIntrouvableException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitIntrouvableException.java
@@ -3,6 +3,7 @@ package com.ecommerce.microcommerce.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProduitIntrouvableException extends RuntimeException {
 
